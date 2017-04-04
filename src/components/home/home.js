09d4eac9906d7b0fcd835480fdf2b47abd8635a5
elsvit <butenko.yv@gmail.com>
2017-04-04 22:08:42 +0300

    3ver

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -13,8 +13,14 @@ var Home = React.createClass({
                 <div className="App">
                     <div className="App-header">
                         <h2>Welcome to React</h2>
+                        <p>New Scting Andrey</p>
                     </div>
                     <p>ERROR</p>
+                    <p>ERROR2</p>
+					<div className="App-string">
+						<h4>Some string</h4>
+						<h4>Some string 2</h4>
+					</div>
                     <p className="App-intro">
                         <button onClick={this.onClickHandler}>Get Started</button>
                     </p>
